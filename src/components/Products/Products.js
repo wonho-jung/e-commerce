f30f84deae9d981967f6e74a2c25cfda6ed900e9
wonho-jung <wonho.jung92@gmail.com>
2021-06-02 14:32:06 -0400

    before grid

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,18 +1,144 @@
 import React from "react";
 import styled from "styled-components";
-import banana from "../../assets/menu.jpg";
+import menu from "../../assets/menu.jpg";
+import avocado from "../../assets/avocadoShake.png";
+import { Button } from "@material-ui/core";
 function Products() {
-  return <ProductsContainer></ProductsContainer>;
+  return (
+    <ProductsContainer>
+      <ProductFirstRow>
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>{" "}
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>
+      </ProductFirstRow>
+      <ProductSecondRow>
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>{" "}
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>
+        <ProductCard>
+          <img src={avocado} alt="" />
+          <Content>
+            <h3>Avocado Juice</h3>
+            <p>Avocado, green veg, apple</p>
+            <h5>$5,99</h5>
+            <Button>ADD TO CART</Button>
+          </Content>
+        </ProductCard>
+      </ProductSecondRow>
+    </ProductsContainer>
+  );
 }
 
 export default Products;
 
 const ProductsContainer = styled.div`
-  background: url(${banana});
-  height: 100vh;
+  background: url(${menu});
+  width: 100%;
+  height: 120vh;
+  object-fit: contain;
   background-size: cover;
-  opacity: 0.8;
-  h1 {
-    opacity: 1;
+  background-position: center;
+  @media screen and (max-width: 1200px) {
+    height: 100vh;
   }
+  @media screen and (max-width: 1050px) {
+    height: 150vh;
+  }
+`;
+const ProductCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    max-width: 300px;
+    @media screen and (max-width: 1200px) {
+      max-width: 200px;
+    }
+  }
+`;
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  button {
+    margin-top: 1rem;
+    color: #fff;
+    border-radius: 20px;
+    padding: 10px;
+    background: #fa4e5c;
+    :hover {
+      color: #fa4e5c;
+      border: 1px solid #fa4e5c;
+    }
+  }
+`;
+const ProductFirstRow = styled.div`
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 1050px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+const ProductSecondRow = styled.div`
+  display: flex;
+  justify-content: center;
 `;
